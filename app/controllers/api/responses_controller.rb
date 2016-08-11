e94b46fe9d9a31b6d49ae95acf9fba3768624315
Derek <dcshiller@gmail.com>
2016-08-10 20:05:26 -0400

    move params out of hash

--- app/controllers/api/responses_controller.rb
+++ app/controllers/api/responses_controller.rb
@@ -2,7 +2,6 @@ class Api::ResponsesController < ApplicationController
 
   # {"response"=>{"0"=>{"name"=>"256", "value"=>"sdfsdfds"}, "1"=>{"name"=>"257", "value"=>"480"}, "2"=>{"name"=>"260", "value"=>"490"}, "3"=>{"name"=>"260", "value"=>"492"}, "4"=>{"name"=>"260", "value"=>"494"}, "5"=>{"name"=>"269", "value"=>"521"}}, "controller"=>"api/responses", "action"=>"create"}
 
-
   def create
     @form = Form.includes(:fields, :choices).find(params[:id])
     responses = params[:responses]
@@ -17,9 +16,8 @@ class Api::ResponsesController < ApplicationController
       end
       {form_field_id: form_field_id, response_value: response_value}
     end
-  end
 
-  new_response_props = {form_id: @form["id"], field_responses_attributes: response_hashes}
+  new_response_props = {form_id: params[:id], field_responses_attributes: response_hashes}
   @new_response = FormResponse.new(new_response_props)
   if @new_response.save
     render :show
