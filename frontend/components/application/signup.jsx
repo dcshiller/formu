38cd66e6a64312d4328165daac2bd36ca918b3c3
Derek <dcshiller@gmail.com>
2016-08-11 13:41:03 -0400

    fix sizing issues

--- frontend/components/application/signup.jsx
+++ frontend/components/application/signup.jsx
@@ -16,7 +16,7 @@ const Signup = React.createClass({
 
   componentDidMount () {
     this.errorStoreReceipt = ErrorStore.addListener(this.newErrors);
-    this.sessiontoreReceipt = SessionStore.addListener(this.newUserHandler);
+    this.sessionStoreReceipt = SessionStore.addListener(this.newUserHandler);
   },
 
   componentWillUnmount () {
@@ -72,14 +72,14 @@ const Signup = React.createClass({
     return (
         <div className="signup">
 
-        <h1>Create usable forms today*.</h1>
+        <h1>Create usable forms today.</h1>
 
         <sidebar>
           <h2>Features</h2>
 
           <ul>
-            <li>You can log in.</li>
-            <li></li><li></li><li></li><li></li><li></li><li></li><li></li>
+            <li>User accounts</li>
+            <li>Dynamic form design interface</li><li>Response view portal</li><li></li><li></li><li></li><li></li><li></li>
             <li>Stay tuned for more features.</li>
           </ul>
         </sidebar>
@@ -123,7 +123,6 @@ const Signup = React.createClass({
           {this.fieldErrors("username")}
           <button onClick={this.signup}>SIGN UP FREE</button>
         </form>
-        <p>*Or by 8/12</p>
       </div>
     )
 
