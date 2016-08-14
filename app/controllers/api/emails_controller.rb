9e5ef0edf199a5a9a8c1c2526a55deff854dc685
Derek <dcshiller@gmail.com>
2016-08-14 16:28:13 -0400

    hook up action mailer

--- app/controllers/api/emails_controller.rb
+++ app/controllers/api/emails_controller.rb
@@ -3,12 +3,13 @@ class Api::EmailsController < ApplicationController
   def invitational_email
     first_field = params["emailParams"].values[0]
     accum_start = {first_field["name"] => first_field["value"]}
-    messaged_params = params["emailParams"].values[1..-1].inject(accum_start) {|accum, field| accum.merge!({field["name"] => field["value"]}) }
-    invitation = FormInvitationMailer.invitation_email(messaged_params["email"],
-                                          messaged_params["recipient"],
-                                          messaged_params["sender"],
-                                          messaged_params["custom_message"],
-                                          params["path"])
+    massaged_params = params["emailParams"].values[1..-1].inject(accum_start) {|accum, field| accum.merge!({field["name"] => field["value"]}) }
+    debugger
+    invitation = FormInvitationMailer.invitation_email(massaged_params["email"],
+                                          massaged_params["recipient"],
+                                          massaged_params["sender"],
+                                          massaged_params["custom_message"],
+                                          massaged_params["path"])
     if invitation.deliver
       render json: {email: "delivered"}
     else
