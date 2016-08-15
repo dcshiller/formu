c592ae391c319376e42d5bb5931f946bc9ddd92b
Derek <dcshiller@gmail.com>
2016-08-15 08:03:00 -0400

    fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  # config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = true
   # config.action_mailer.default_url_options = { host: 'formu.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
