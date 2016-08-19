d97c00d7149490d86aa7623731dc2dedab21ca4f
Derek <dcshiller@gmail.com>
2016-08-19 08:47:50 -0400

    fiddle with email conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,15 +34,15 @@ Rails.application.configure do
   #   :domain         => 'formu.derekshiller.com',
   #   :authentication => :plain,
   # }
-    ActionMailer::Base.smtp_settings = {
-  :port           => '587', #ENV['MAILGUN_SMTP_PORT'],
-  :address        =>  'smtp.gmail.com',  #ENV['MAILGUN_SMTP_SERVER'],
-  :user_name      => 'dcshiller', #ENV['MAILGUN_SMTP_LOGIN'],
-  :password       =>  ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'formu.derekshiller.com',
-  :authentication => :plain,
-}
-
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 #   ActionMailer::Base.smtp_settings = {
 #   :port           => '587', #ENV['MAILGUN_SMTP_PORT'],
 #   :address        =>  '209.61.151.224',  #ENV['MAILGUN_SMTP_SERVER'],
