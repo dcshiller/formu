dbf5e75d5a2337e98ed46b983946a3f7d4aa2fbe
Derek <dcshiller@gmail.com>
2016-08-21 15:10:48 -0400

    remove comments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,9 +13,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  # config.action_mailer.default_url_options = { host: 'formu.herokuapp.com' }
-  # config.action_mailer.delivery_method = :mailgun
-  # config.action_mailer.mailgun_settings = {domain: '.mailgun.org'}
+
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
